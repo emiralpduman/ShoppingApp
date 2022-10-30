992767e36e4870c08c4c3504e583fa996eec932b
Emiralp <emiralpduman@gmail.com>
2022-10-30 23:57:38 +0300

    ProductsView is being developed.

---
+++ Shopping App/Screens/Products/View/ProductsView.swift
@@ -0,0 +1,46 @@
+//
+//  ProductsView.swift
+//  Shopping App
+//
+//  Created by Emiralp Duman on 30.10.2022.
+//
+
+import UIKit
+
+class ProductsView: UIView {
+    
+    //MARK: -Properties
+    
+    var products: [Product]?
+    
+    //MARK: - Subviews
+    
+    private lazy var productsCollectionView = UICollectionView()
+    
+    //MARK: -Init
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .red
+        setupProductsCollectionView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - Methods
+    
+    //Subview Building Methods
+    private func setupProductsCollectionView() {
+        productsCollectionView.register(ProductsCollectionViewCell.self, forCellWithReuseIdentifier: "productsCell")
+        addSubview(productsCollectionView)
+        productsCollectionView.snp.makeConstraints { make in
+            make.top.equalToSuperview()
+            make.bottom.equalToSuperview()
+            make.leading.equalToSuperview()
+            make.trailing.equalToSuperview()
+        }
+    }
+}
