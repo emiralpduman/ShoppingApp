6cc5d8e2be38feeacba1399b936e53b76d8d7e06
Emiralp <emiralpduman@gmail.com>
2022-10-30 22:34:46 +0300

    Folder structure is initiated

---
+++ Shopping App/App/Delegates/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  Shopping App
+//
+//  Created by Emiralp Duman on 30.10.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+
+}
+
