af417643df77d4337eb17b85f78c8e8851a610e4
Emiralp <emiralpduman@gmail.com>
2022-10-31 17:59:40 +0300

    "ProductsCollectionViewCell" development is done.

--- Shopping App/Screens/Products/Controller/ProductsViewController.swift
+++ Shopping App/Screens/Products/Controller/ProductsViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ProductsViewController: UIViewController {
+final class ProductsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
