c2c276869a9f9b2834acc15284313a4c1a8979e7
Emiralp <emiralpduman@gmail.com>
2022-11-01 19:31:41 +0300

    Gradient layer is developed

--- Shopping App/Screens/Products/Model/Product.swift
+++ Shopping App/Screens/Products/Model/Product.swift
@@ -10,5 +10,5 @@ import Foundation
 struct Product {
     var name: String = "Product name"
     var price: Double = 999.99
-    var imageURL: URL = URL(string: "https://www.sesinoks.com.tr/wp-content/uploads/default-placeholder.png")!
+    var imageURL: URL = URL(string: "https://cdn.cimri.io/image/1000x1000/sonyplaystationoyunkonsolu_622618971.jpg")!
 }
