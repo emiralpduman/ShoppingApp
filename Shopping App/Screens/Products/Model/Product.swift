eab9114df6efeca8a0c80d5dc718407deb948164
Emiralp <emiralpduman@gmail.com>
2022-11-01 23:38:30 +0300

    Product detail page is done.

--- Shopping App/Screens/Products/Model/Product.swift
+++ Shopping App/Screens/Products/Model/Product.swift
@@ -7,8 +7,18 @@
 
 import Foundation
 
-struct Product {
-    var name: String = "Product name"
-    var price: Double = 999.99
-    var imageURL: URL = URL(string: "https://cdn.cimri.io/image/1000x1000/sonyplaystationoyunkonsolu_622618971.jpg")!
+struct Product: Codable {
+    var id: Int = 0
+    var title: String = "Product's name"
+    var price: Double = 99.99
+    var description: String = "Product's description"
+    var category: String = "Product's category"
+    var imageURL: String = "https://cdn.cimri.io/image/1000x1000/sonyplaystationoyunkonsolu_622618971.jpg"
+    var rating: Rating = Rating()
 }
+
+struct Rating: Codable {
+    var rate: Double = 9.9
+    var count: Int = 99
+}
+
