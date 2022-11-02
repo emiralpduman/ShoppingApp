5aa6ce45d6df28c233608881c791906250340e47
Emiralp <emiralpduman@gmail.com>
2022-11-02 14:26:24 +0300

    Modal presentation of quantity is added.

--- Shopping App/Screens/Products/View/AddToCartView.swift
+++ Shopping App/Screens/Products/View/AddToCartView.swift
@@ -6,15 +6,69 @@
 //
 
 import UIKit
+import SnapKit
 
-class AddToCartView: UIView {
+protocol AddToCartViewDelegate: AnyObject {
+    func addToCartView(_ view: AddToCartView, didTapStepper: UIStepper)
+}
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+final class AddToCartView: UIView {
+    
+    // MARK: - Properties
+    
+    weak var delegate: AddToCartViewDelegate?
+    
+    var quantity: Int = 0 {
+        didSet {
+            quantityLabel.text = "Amount: \(quantity)"
+        }
     }
-    */
+    
+    // MARK: - Visual Elements
+    
+    private lazy var quantityStepper: UIStepper = {
+        let stepper = UIStepper()
+        stepper.minimumValue = 0
+        stepper.addTarget(self, action: #selector(didTapQuantityStepper), for: .valueChanged)
+        return stepper
+    }()
+    private lazy var quantityLabel: UILabel = {
+       let label = UILabel()
+        label.text = "Amount: \(quantity)"
+        return label
+    }()
+    private lazy var quantityStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [quantityLabel, quantityStepper])
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        stackView.alignment = .center
+        stackView.spacing = stackViewSpacing
+        return stackView
+    }()
+    
+    // MARK: - Initilization
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        //Adding subviews
+        addSubview(quantityStackView)
+        quantityStackView.snp.makeConstraints() { make in
+            make.center.equalToSuperview()
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Delegate Methods
+    
+    @objc private func didTapQuantityStepper(_ sender: UIStepper) {
+        delegate?.addToCartView(self, didTapStepper: sender)
+    }
+    
+    //Drawing constants
+    private let stackViewSpacing: CGFloat = 10
 
 }
