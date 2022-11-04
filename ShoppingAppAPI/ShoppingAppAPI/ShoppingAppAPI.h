97412aa2a8e1be7ee8d00df4acb12587b7674383
Emiralp <emiralpduman@gmail.com>
2022-11-04 12:14:47 +0300

    Develop and network feature branch is merged

---
+++ ShoppingAppAPI/ShoppingAppAPI/ShoppingAppAPI.h
@@ -0,0 +1,18 @@
+//
+//  Shopping_App_API.h
+//  Shopping App API
+//
+//  Created by Emiralp Duman on 3.11.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Shopping_App_API.
+FOUNDATION_EXPORT double ShoppingAppAPIVersionNumber;
+
+//! Project version string for Shopping_App_API.
+FOUNDATION_EXPORT const unsigned char ShoppingAppAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Shopping_App_API/PublicHeader.h>
+
+
