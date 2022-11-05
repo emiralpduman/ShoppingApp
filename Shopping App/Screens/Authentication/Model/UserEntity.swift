577c7616c3040d5df6a2bfab857bbbb89c56df94
Emiralp <emiralpduman@gmail.com>
2022-11-05 16:16:02 +0300

    Firebase Auth, realmDB integration is established.

---
+++ Shopping App/Screens/Authentication/Model/UserEntity.swift
@@ -0,0 +1,22 @@
+//
+//  UserEntity.swift
+//  Shopping App
+//
+//  Created by Emiralp Duman on 5.11.2022.
+//
+
+import Foundation
+import RealmSwift
+
+class UserEntity: Object {
+    @Persisted(primaryKey: true) var _id: String
+    @Persisted var userName: String
+    @Persisted var emailAddress: String
+    @Persisted var cart: List<OrderEntity>
+}
+
+class OrderEntity: Object {
+    @Persisted(primaryKey: true) var _id: String
+    @Persisted var productId: String
+    @Persisted var amount: Int
+}
