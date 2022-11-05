804caaa4ed33a6e1300fb83205853ba6e74dbbdf
Emiralp <emiralpduman@gmail.com>
2022-11-05 22:30:26 +0300

    Initial folder structure

---
+++ Shopping App/Screens/Main/ViewController/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  Shopping App
+//
+//  Created by Emiralp Duman on 5.11.2022.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
