09d42587f14d1df8a430d5154944e7eb4650bcd1
Emiralp <emiralpduman@gmail.com>
2022-11-05 22:30:26 +0300

    Initial folder structure

---
+++ Shopping App/Screens/Main/View/MainView.swift
@@ -0,0 +1,20 @@
+//
+//  MainView.swift
+//  Shopping App
+//
+//  Created by Emiralp Duman on 5.11.2022.
+//
+
+import UIKit
+
+final class MainView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Shopping App/Screens/Main/ViewModel/MainViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  MainViewModel.swift
+//  Shopping App
+//
+//  Created by Emiralp Duman on 5.11.2022.
+//
+
+import Foundation
+
+final class mainViewModel {
+    
+}
