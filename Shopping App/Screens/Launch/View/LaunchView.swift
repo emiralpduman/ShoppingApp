91c68d1dc86c9afd5b1af6d14d71e3713a92cb72
Emiralp <emiralpduman@gmail.com>
2022-11-06 13:11:40 +0300

    AppIcon was added

--- Shopping App/Screens/Launch/View/LaunchView.swift
+++ Shopping App/Screens/Launch/View/LaunchView.swift
@@ -6,11 +6,12 @@
 //
 
 import UIKit
+import SnapKit
 
 class LaunchView: UIView {
     // MARK: - Subviews
     private lazy var appIconImageView: UIImageView = {
-        let imageView = UIImageView(image: UIImage())
+        let imageView = UIImageView(image: UIImage(named: "AppIcon"))
         
         return imageView
     }()
@@ -19,7 +20,41 @@ class LaunchView: UIView {
         
         return indicator
     }()
+    
+    // MARK: - Initialization
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupAppIconImageView()
+        setupActivityIndicatorView()
+        activityIndicatorView.startAnimating()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
 
-
+    // MARK: - Methods
+    private func setupAppIconImageView() {
+        addSubview(appIconImageView)
+        
+        appIconImageView.snp.makeConstraints() { make in
+            make.center.equalToSuperview()
+        }
+    }
+    
+    private func setupActivityIndicatorView() {
+        addSubview(activityIndicatorView)
+        
+        activityIndicatorView.snp.makeConstraints() { make in
+            make.centerX.equalTo(appIconImageView)
+            make.top.equalTo(appIconImageView.snp.bottom).offset(activityIndicatorOffset)
+        }
+        
+    }
+    
+    // MARK: - Drawing Constant
+    private let activityIndicatorOffset: CGFloat = 10
 
 }
