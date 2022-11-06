9bc66c6e484bef6129a01c5bbd3c4357dd2ec6b1
Emiralp <emiralpduman@gmail.com>
2022-11-06 12:36:10 +0300

    Folder structure established and initial view-controller files are created

---
+++ Shopping App/Screens/Launch/ViewController/LaunchViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LaunchViewController.swift
+//  Shopping App
+//
+//  Created by Emiralp Duman on 6.11.2022.
+//
+
+import UIKit
+
+class LaunchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
