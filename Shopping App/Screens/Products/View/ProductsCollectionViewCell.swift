d020cf415f553cd66e1959a51dd7949b326b9c2b
Emiralp <emiralpduman@gmail.com>
2026-02-24 15:48:06 +0300

    feat: Add product detail view with image gradient overlay

--- Shopping App/Screens/Products/View/ProductsCollectionViewCell.swift
+++ Shopping App/Screens/Products/View/ProductsCollectionViewCell.swift
@@ -9,11 +9,16 @@ import UIKit
 import Kingfisher
 
 final class ProductsCollectionViewCell: UICollectionViewCell {
+    // MARK: - Drawing Constants
+    let cornerRadius: CGFloat = 50
+    let labelHorizontalInsetWRTImage: CGFloat = 40
+    let labelVerticalInsetWRTImage: CGFloat = 10
+    
     //MARK: - Properties
     var product: Product {
         didSet {
-            productImageView.kf.setImage(with: product.imageURL)
-            productNameLabel.text = product.name
+            productImageView.kf.setImage(with: URL(string: product.imageURL ))
+            productNameLabel.text = product.title
             productPriceLabel.text = String(product.price)
         }
     }
@@ -21,26 +26,29 @@ final class ProductsCollectionViewCell: UICollectionViewCell {
     //MARK: - Visual Elements
     private lazy var productImageView: UIImageView = {
         let imageView = UIImageView()
+        imageView.layer.cornerRadius = cornerRadius
+        imageView.contentMode = .scaleAspectFit
         imageView.clipsToBounds = true
         return imageView
     }()
     private lazy var productNameLabel: UILabel = {
         let nameLabel = UILabel()
+        nameLabel.textColor = .white
         return nameLabel
     }()
     private lazy var productPriceLabel: UILabel = {
         let priceLabel = UILabel()
+        priceLabel.textColor = .white
         return priceLabel
     }()
     private lazy var gradientLayer: CAGradientLayer = {
         let layer = CAGradientLayer()
         layer.startPoint = CGPoint(x: 0.5, y: 0.0)
         layer.endPoint = CGPoint(x: 0.5, y: 1.0)
-        layer.colors = [UIColor.red.cgColor, UIColor.blue.cgColor]
+        layer.colors = [UIColor.clear.cgColor, UIColor.black.cgColor]
         layer.locations = [0.0, 1.0]
         return layer
     }()
-    private lazy var gradientView = UIView()
     
     //MARK: - Initialization
     override init(frame: CGRect) {
@@ -52,49 +60,36 @@ final class ProductsCollectionViewCell: UICollectionViewCell {
         addSubview(productImageView)
         productImageView.snp.makeConstraints { make in
             make.top.equalToSuperview()
-            make.leading.equalToSuperview()
-            make.trailing.equalToSuperview()
+            make.leading.equalToSuperview().inset(labelHorizontalInsetWRTImage/3)
+            make.trailing.equalToSuperview().inset(labelHorizontalInsetWRTImage/3)
             make.bottom.equalToSuperview()
         }
         
         addSubview(productNameLabel)
         productNameLabel.snp.makeConstraints { make in
-            make.leading.equalTo(productImageView.snp.leading)
-            make.bottom.equalTo(productImageView.snp.bottom)
+            make.leading.equalTo(productImageView.snp.leading).inset(labelHorizontalInsetWRTImage)
+            make.bottom.equalTo(productImageView.snp.bottom).inset(labelVerticalInsetWRTImage)
         }
         
         addSubview(productPriceLabel)
         productPriceLabel.snp.makeConstraints { make in
             make.leading.equalTo(productNameLabel.snp.trailing).offset(10)
-            make.trailing.equalToSuperview()
+            make.trailing.equalToSuperview().inset(labelHorizontalInsetWRTImage)
             make.top.equalTo(productNameLabel.snp.top)
             make.bottom.equalTo(productNameLabel.snp.bottom)
         }
         
-        addSubview(gradientView)
-        gradientView.snp.makeConstraints { make in
-            make.leading.equalToSuperview()
-            make.top.equalToSuperview()
-            make.trailing.equalToSuperview()
-            make.bottom.equalToSuperview()
-        }
-        
+         productImageView.layer.insertSublayer(gradientLayer, at: .zero)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    override func draw(_ rect: CGRect) {
-        let layer = CAGradientLayer()
-        layer.startPoint = CGPoint(x: 0.5, y: 0.0)
-        layer.endPoint = CGPoint(x: 0.5, y: 1.0)
-        layer.colors = [UIColor.red.cgColor, UIColor.blue.cgColor]
-        layer.locations = [0.0, 1.0]
-        gradientView.layer.addSublayer(layer)
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        gradientLayer.frame = bounds
     }
-    
-    
 }
 
 
